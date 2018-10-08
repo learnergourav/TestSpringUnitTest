ce028515ee946dccd776f7283ac819c363b96fed
Gourav <learnergourav@gmail.com>
2018-10-08 16:01:37 -0400

    Project Added to GIT.

---
+++ src/main/java/com/basicunittest/SpringBasicUniteTestBootConfig.java
@@ -0,0 +1,12 @@
+package com.basicunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicUniteTestBootConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicUniteTestBootConfig.class, args);
+	}
+}
