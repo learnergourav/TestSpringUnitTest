d6eb8a8f029f0386907175b008169c661e9ef54c
Gourav <learnergourav@gmail.com>
2018-10-08 22:56:56 -0400

    Updated file.

--- src/main/java/com/basicunittest/controller/TestController.java
+++ src/main/java/com/basicunittest/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.basicunittest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	@RequestMapping("/healthcheck")
+	public void HealthCheck() {
+		System.out.println("Health Check");
+	}
 }
